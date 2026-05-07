0465666eead3838404d5a30a7653443dcf19f29f
morelyannick <morelyannick92@gmail.com>
2026-05-07 02:13:56 +0000

    Add initial HTML structure, CSS styles, and JavaScript for dynamic footer

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year and display it in the footer
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// Get the last modified date and display it in the footer
+document.getElementById("lastModified").innerHTML = document.lastModified;
